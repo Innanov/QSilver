1f07ead417651e047bb829b9170c21d799c05ce2
INNAN Nouhaila <64653897+Innanov@users.noreply.github.com>
2021-05-26 11:48:43 +0100

    Delete Introduction to Complex Numbers directory

--- Introduction to Complex Numbers/task1.py
+++
@@ -1,8 +0,0 @@
-from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
-qreg =  QuantumRegister(10) 
-creg = ClassicalRegister(10)
-circuit = QuantumCircuit(qreg,creg)
-circuit.h(qreg[0])
-for i in range(1,10):
-    circuit.cx(qreg[0],qreg[i])
-circuit.draw()
